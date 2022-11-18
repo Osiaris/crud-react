d40483da7d1fef97686add8d1450a4b11a6d73c8
Manne <manneh@stortand.se>
2022-11-18 11:55:42 +0100

    Hejsan

--- src/Components/firebase/crudContext.js
+++ src/Components/firebase/crudContext.js
@@ -2,57 +2,59 @@ import React, { useContext, useState, useEffect } from 'react';
 import { getDatabase, push, ref, set, child, get } from 'firebase/database';
 import database from './config';
 
-const CardData = React.createContext()
-const UpdateCardData = React.createContext() 
+const CardData = React.createContext();
+const UpdateCardData = React.createContext();
 
-
-export const useCardData = () =>{
+export const useCardData = () => {
     return useContext(CardData);
-} 
+};
 
-export const useUpdateCardData = () =>{
+export const useUpdateCardData = () => {
     return useContext(UpdateCardData);
-} 
+};
 
-export const CardDataContext = ({children}) => {
-   const [currentData, setCurrentData] = useState([]);
-   
-   const appendToDB = (title, post) => {
-      const db = getDatabase();
-      const postListRef = ref(db, 'test4');
-      const newPostRef = push(postListRef);
-      const data = {
-         title: title,
-         post: post,
-         id: newPostRef.key,
-     }
-      set(newPostRef, data);
-      setCurrentData = appendToDB(title, post);
-      return data;
-  };
+export const CardDataContext = ({ children }) => {
+    const [currentData, setCurrentData] = useState([]);
 
-   
-//   useEffect(() => {
-//         const dbRef = ref(getDatabase());
-//         get(child(dbRef, 'cards/'))
-//             .then((snapshot) => {
-//                 if (snapshot.exists()) {
-//                     const data = snapshot.val();
-//                 } else {
-//                     return [];
-//                 }
-//             })
-//             .catch((error) => {
-//                 console.error(error);
-//             });
-//   },[])
+    const appendToDB = (title, post) => {
+        const db = getDatabase();
+        const postListRef = ref(db, 'test5');
+        const newPostRef = push(postListRef);
+        const data = {
+            title: title,
+            post: post,
+            id: newPostRef.key,
+        };
+        set(newPostRef, data);
+        setCurrentData((prev) => [...prev, data]);
+        return data;
+    };
+
+    const fetchDB = async () => {
+        try {
+            const dbRef = ref(getDatabase());
+            const snapshot = await get(child(dbRef, 'test5/'));
+            if (snapshot.exists()) {
+                setCurrentData(Object.values(snapshot.val()));
+                console.log(Object.values(snapshot.val()));
+                console.log(currentData);
+            } else {
+                return [];
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchDB();
+    }, []);
 
     return (
-      <CardData.Provider value={currentData}>
-        <UpdateCardData.Provider value={appendToDB}>
-           {children}
-        </UpdateCardData.Provider>
-      </CardData.Provider>
-   );
-}
-
+        <CardData.Provider value={currentData}>
+            <UpdateCardData.Provider value={appendToDB}>
+                {children}
+            </UpdateCardData.Provider>
+        </CardData.Provider>
+    );
+};
